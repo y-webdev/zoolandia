538a28434089933a57c304ec66aa325e94aa52b5
Yanislav Pavlov <yanislav.pavlov@kyto.com>
2021-04-17 13:35:18 +0200

    Add static data

---
+++ src/composable/staticData.js
@@ -0,0 +1,56 @@
+export const websiteInfo = {
+    name: 'Зооландия',
+    company: 'Ветеринарна клиника',
+    logo: 'zoolandia-logo.svg'
+}
+
+export const facebookPageInfo = {
+    url: 'https://www.facebook.com/zoolandiabg',
+    title: 'Фейсбук страница на Зооландия'
+}
+
+export const navItems = [
+    {
+        link: '#about',
+        title: 'За нас'
+    },
+    {
+        link: '#services',
+        title: 'Услуги'
+    },
+    {
+        link: '#prices',
+        title: 'Цени'
+    },
+    {
+        link: '#subscription',
+        title: 'Абонамент'
+    },
+    {
+        link: 'http://www.petzona.net',
+        title: 'Магазин'
+    },
+    {
+        link: '#contact',
+        title: 'Контакти'
+    }
+]
+
+export const webmaster = {
+    text: 'Уеб дизайн и програмиране',
+    url: 'https://www.yweb.dev',
+    name: 'yweb.dev',
+    logo: 'yweb-dev-logo-sm.svg'
+}
+
+export const priceNavItems = {
+    reviews:'Прегледи',
+    vaccines:'Ваксини',
+    deworm:'Обезпаразитяване',
+    hospitalization:'Хоспитализация',
+    procedures:'Мед. процедури',
+    surgery:'Хирургия',
+    dentistry:'Стоматология',
+    diagnosis:'Диагностика',
+    others:'Други'
+}
